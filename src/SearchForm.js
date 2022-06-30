7e3bd332c4323d34a349d6a016b64e68779e8207
jieunchung <jieunchung2@gmail.com>
2022-06-30 21:38:32 +0200

    functioning search component

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchForm.css";
 
 export default function SearchForm() {
+  const [keyword, setKeyword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleChange(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="form-container">
-      <form className="form-group mx-sm-3 mb-2">
+      <form className="form-group mx-sm-3 mb-2" onSubmit={handleSubmit}>
         <input
           className="form-control form-input"
           type="search"
           placeholder="nature"
-        ></input>
+          onChange={handleChange}
+        />
         <small className="hints">e.g. summer, kitten, flower, hiking</small>
       </form>
     </div>
