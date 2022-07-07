9c443ea7c7cec01b15681c473836e2e3b7443323
jieunchung <jieunchung2@gmail.com>
2022-07-07 08:07:26 +0200

    error fix with noreferrer

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   if (props.phonetic.audio.length) {
     return (
       <div>
-        <a href={props.phonetic.audio} target="_blank">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
           Listen
         </a>
         {props.phonetic.text}
